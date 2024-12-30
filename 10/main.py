0357c2479cc7728f45c0d460aac0bebc6cfb7b1f
bane80ns <bane80@gmail.com>
2024-12-30 19:28:52 +0100

    * Renamed data.json >> user.json * Created folder "data" * moved user.json to "data" folder * Created empty products.json inside "data" folder * Changed references in main.py

--- 10/main.py
+++ 10/main.py
@@ -1,7 +1,7 @@
 import json
 
 # json file loading "r" - read
-with open("data.json", "r") as file:
+with open("data/user.json", "r") as file:
     data = json.load(file)
     data.append({
         "name": "Petar Petrovic",
@@ -13,5 +13,5 @@ with open("data.json", "r") as file:
 print(data)
 
 # jason file write "w"
-with open("data.json", "w") as file:
+with open("data/user.json", "w") as file:
     json.dump(data, file, indent=4)
