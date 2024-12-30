f9c903d096bb199ff369aba88ca05e7f901eeb70
bane80ns <bane80@gmail.com>
2024-12-30 19:00:24 +0100

    Finished main python and data.json

--- 10/main.py
+++ 10/main.py
@@ -1,2 +1,17 @@
-# test test2
-# Another test for commit and push
+import json
+
+# json file loading "r" - read
+with open("data.json", "r") as file:
+    data = json.load(file)
+    data.append({
+        "name": "Petar Petrovic",
+        "age": 35,
+        "height": 190,
+        "gender": "male"
+    })
+
+print(data)
+
+# jason file write "w"
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
